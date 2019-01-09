02ea3d51c99c222f69c8124d9d15ea5eb8738cb4
Andreas Jakl <andreas.jakl@live.com>
2019-01-09 19:03:59 +0100

    Code basic expres server and install socket io

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-console.log("Hello world");
+const express = require('express')
+const app = express()
 
+app.use(express.static('public'))
 
-// TODO:  Write code here
+// http://127.0.0.1/
+//app.get('/', (req, res) => {
+//    res.send("Hello world");
+//})
+
+app.listen(3000, () => console.log("Example app running"))

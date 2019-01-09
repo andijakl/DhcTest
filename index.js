5dc1c5c759b39266c065c98f685abdb16be1bd40
Andreas Jakl <andreas.jakl@live.com>
2019-01-09 20:05:00 +0100

    Added messages from the client to the server

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app = express()
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static('public'))
+
+io.on("connection", function (socket) {
+    console.log("Client connected");
+    socket.on('UserInput', function (msg) {
+        console.log("Received message: " + msg);
+    })
+})
 
 // http://127.0.0.1/
 //app.get('/', (req, res) => {
 //    res.send("Hello world");
 //})
 
-app.listen(3000, () => console.log("Example app running"))
+http.listen(3000, () => console.log("Example app running"))
--- public/logic.js
+++ public/logic.js
@@ -1,10 +1,16 @@
 $(document).ready(function () {
     console.log("Document ready");
 
+    var socket = io();
+
     $("#textform").submit(function(e) {
+        // Get the user entered text
         const enteredText = $("#usertext").val();
         console.log("Form submitted - entered text: " + enteredText);
         
+        // Send the message to the server via socket.io
+        socket.emit('UserInput', enteredText);
+
         e.preventDefault();
     })
 })
